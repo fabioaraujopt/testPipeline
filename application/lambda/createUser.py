e971d4732be05f0ddc720de7b59598e178445e53
Fábio Araújo <fabio.araujo@outsystems.com>
2020-04-14 15:35:15 +0100

    add last changes

--- application/lambda/createUser.py
+++ application/lambda/createUser.py
@@ -9,13 +9,13 @@ def lambda_handler(event, context):
 
     eventBody = json.loads(event["body"])
 
-    #try:
-    response = createCloudWatchAccount(eventBody["accountId"],eventBody["username"])
-    #except botocore.exceptions.ClientError as e:
-    #    return {
-    #        'statusCode': 400,
-    #        'body': json.dumps(errorResponse(e))
-    #    }
+    try:
+        response = createCloudWatchAccount(eventBody["accountId"],eventBody["username"])
+    except botocore.exceptions.ClientError as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(errorResponse(e))
+        }
 
     return {
         'statusCode': 200,
