9b1b3956e57961d37c225aeb143cb532f9aaad44
Fábio Araújo <fabio.araujo@outsystems.com>
2020-04-14 10:49:44 +0100

    asas

--- application/lambda/errorResponse.py
+++ application/lambda/errorResponse.py
@@ -4,14 +4,19 @@ import json
 def errorResponse(exception):
 
     errorCode = exception.response['Error']['Code']
+    error_message = exception.response['Error']['Message']
 
     if(errorCode == "AccessDenied"):
         error_message = "Access denied for account"
     if(errorCode == "EntityAlreadyExists"):
         error_message = "Username already exists"
+    if(errorCode == "NoSuchEntity"):
+        error_message = "Username do not exists"
+        
     else:
         error_message = errorCode
 
     return {
+        "error_code": errorCode,
         "error_message": error_message
     }
