87e1e4e7938e317a4a0c683ed954b68b56c7a2a2
Fábio Araújo <fabio.araujo@outsystems.com>
2020-04-17 10:09:55 +0100

    add comment

--- application/lambda/updatePolicy.py
+++ application/lambda/updatePolicy.py
@@ -34,7 +34,7 @@ def updateCloudWatchPolicy(AWSAccountId):
 
     with open('./policies/CWUser.json') as f:
             repoPolicy = json.load(f)
-    #se policy está igual não atualiza....
+    #se policy está igual não atualiza....aw
     try:
         policy = iam.Policy(policyArn).load()
         policy.create_version(
