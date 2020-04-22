3dad4384123136b2c8f93969c767c674fd336e50
Fábio Araújo <fabio.araujo@outsystems.com>
2020-04-22 11:44:22 +0100

    change regex

--- application/lambda/auth/authorizer.py
+++ application/lambda/auth/authorizer.py
@@ -35,7 +35,7 @@ def lambda_handler(event, context):
 
     method = api_gateway_arn_tmp[2]
 
-    resource = m = re.search('cloudwatch\/.+$', method_arn)
+    resource = re.search('\d\/.+$', method_arn)
 
     aws_account_id = tmp[4]
 
